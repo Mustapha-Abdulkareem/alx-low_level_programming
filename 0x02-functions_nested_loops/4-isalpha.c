cfef419dd505b704ab47a87c311139d933d099aa
Mustapha-Abdulkareem <mustaphaabdulkareem22@gmail.com>
2022-09-18 11:53:56 -0700

    Hot fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  *@c: is the int that will be use for the argument of the function
  *Return: 0
  */
-int _isalpha(int c)
+int_isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
